1c4fa4502caf40d4b4b0f60b538858c7a2680ae9
yushicheng <1542874601@qq.com>
2019-12-19 23:20:16 +0800

    发布版本,并上传到gitee

--- scripts/git-test.js
+++ scripts/git-test.js
@@ -5,8 +5,12 @@ const workPath = path.resolve(__dirname, "../");
 const git = simpleGit(workPath);
 
 (async function () {
-  console.log(await git.init());
-  console.log(await git.add("*"));
-  console.log(await git.commit("发布版本,并上传到gitee"));
-  console.log(await git.status());
+  try {
+    console.log(await git.init());
+    console.log(await git.add("*"));
+    console.log(await git.commit("发布版本,并上传到gitee"));
+    console.log(await git.status());
+  } catch (error) {
+    console.log(error);
+  };
 })();
