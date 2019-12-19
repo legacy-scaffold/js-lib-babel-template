58bb540e37f253567b3d4db4c3db1d42cb230a04
yushicheng <1542874601@qq.com>
2019-12-19 23:38:17 +0800

    发布版本,并上传到gitee

--- scripts/build.js
+++ scripts/build.js
@@ -9,23 +9,36 @@ const workPath = path.resolve(__dirname, "../");
 const git = simpleGit(workPath);
 
 
-
+const spinner = ora();
 (async function () {
-  const spinner = ora("正在将js转换成es5").start();
-  spawn.sync("babel", [
-    path.resolve(__dirname, "../src/"),
-    "--out-dir",
-    path.resolve(__dirname, "../dist/")
-  ]);
-  spinner.text = "转换成功!";
-  spinner.text = "正在进行git操作";
   try {
-    console.log("工作区状态=>", await git.status());
+    spinner.start();
+    spinner.text = "正在将js转换成es5"
+    spawn.sync("babel", [
+      path.resolve(__dirname, "../src/"),
+      "--out-dir",
+      path.resolve(__dirname, "../dist/")
+    ]);
+    spinner.text = "转换成功!";
+  } catch (error) {
+    spinner.text = "转换失败!";
+    throw error;
+  } finally {
+    spinner.stop();
+  };
+
+  try {
+    spinner.text = "正在进行git提交操作";
+    await git.status();
     await git.init();
     await git.add("*");
     await git.commit("发布版本,并上传到gitee");
+    spinner.text = "所有文件提交成功!";
   } catch (error) {
-
+    spinner.text = "文件提交失败!";
+    throw error;
+  } finally {
+    spinner.stop();
   };
 })();
 
